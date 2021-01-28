c982ecc4ca99667c4f8bad146899e595c8182a85
Johan Skär Holm <johanskarholm@gmail.com>
2021-01-28 14:24:44 +0100

    Fix anchors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,14 +5,12 @@
   import OpenSource from '../blocks/index/OpenSource.svelte';
   import Offices from '../blocks/index/Offices';
   import Careers from '../blocks/index/Careers';
-  import Tech from '../blocks/index/Tech.svelte';
+  // import Tech from '../blocks/index/Tech.svelte';
   import About from '../blocks/index/About.svelte';
   import Header from '../blocks/index/Header.svelte';
 </script>
 
-<AnchorTarget id="">
-  <Header />
-</AnchorTarget>
+<Header />
 
 <AnchorTarget id="about">
   <About />
