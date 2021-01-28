b10f1f75457bba8f6aab2ebb8dfd98ee0fd5c5b3
Johan Skär Holm <johanskarholm@gmail.com>
2021-01-28 09:45:45 +0100

    Temporaryhide logos on phone

--- src/components/LogoGrid.svelte
+++ src/components/LogoGrid.svelte
@@ -17,17 +17,14 @@
     }
 
     @media (max-width: 600px) {
-      & > :global(*) {
-        height: auto;
-        width: 33.3%;
-      }
+      display: none;
     }
   }
   /* div {
     grid-gap: 1vw;
     display: grid;
     grid-auto-rows: 80px;
-    grid-template-columns: repeat(4, minmax(50px, 1fr));
+    grid-template-columns: repeat(3, minmax(50px, 1fr));
 
     @media (max-width: 600px) {
       grid-template-columns: repeat(3, minmax(50px, 1fr));
